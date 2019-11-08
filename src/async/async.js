d62dd6a108e02098c5788f6239befdce1c3a5695
betterliyu <liyu@betterliyu.site>
2019-11-08 14:01:58 +0800

    test pre-commit lint

--- src/async/async.js
+++ src/async/async.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-function readFile(fileName)  {
+function readFile(fileName) {
   return new Promise((resolve, reject) => {
     fs.readFile(fileName, (error, data) => {
       if (error) return reject(error);
@@ -10,7 +10,7 @@ function readFile(fileName)  {
   });
 }
 
-async function asyncReadFile () {
+async function asyncReadFile() {
   const f1 = await readFile(path.join(__dirname, 'file1.txt'));
   const f2 = await readFile(path.join(__dirname, 'file2.txt'));
   console.log(f1.toString());
@@ -18,8 +18,3 @@ async function asyncReadFile () {
 }
 
 asyncReadFile();
-
-
-
-
-  
