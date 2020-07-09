b1b1866c685fbecd57047de485304cab7068bc10
Li Yu <edenliyu@outlook.com>
2020-07-09 18:11:19 +0800

    nodejs: add express demo

---
+++ Node.js/heima_training/day01/module/require-test.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-useless-path-segments */
+/* eslint-disable import/no-absolute-path */
+/* eslint-disable import/no-extraneous-dependencies */
+require('fs');
+require('foo-outer.js');
+require('foo-outer');
+require('foo');
+require('D:\\training.codes\\Node.js\\heima_training\\day01\\module');
+require('/training.codes/Node.js/heima_training/day01/module');
+require('/training.codes/Node.js/heima_training/day01/module/');
+require('./folder/app.js');
+require('./folder/');
