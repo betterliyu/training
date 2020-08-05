aadd0000a0e45d0f4a8a77e1f48a55ee75d508a4
Li Yu <edenliyu@outlook.com>
2020-08-05 15:09:17 +0800

    algs: add algs C# code

---
+++ algorithm/code/algs-4/Algs4/Algs4.Exercises/Chapter1/Ex_1_3_19.cs
@@ -0,0 +1,28 @@
+﻿namespace Algs4.Exercises
+{
+    public class Ex_1_3_19
+    {
+        public static void DeleteLast(ref Lib.LinkedListNode<int> first)
+        {
+            if (first == null)
+            {
+                return;
+            }
+
+            if (first.next == null)
+            {
+                first = null;
+                return;
+            }
+
+            Lib.LinkedListNode<int> cur = first;
+
+            while (cur.next.next != null)
+            {
+                cur = cur.next;
+            }
+
+            cur.next = null;
+        }
+    }
+}
---
+++ algorithm/code/algs-4/Algs4/Algs4.Lib/LinkedList.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Algs4.Lib
+{
+    public class LinkedList<Item>
+    {
+        public LinkedListNode<Item> first;
+
+        public LinkedList()
+        {
+        }
+
+        public LinkedList(LinkedListNode<Item> first)
+        {
+            this.first = first;
+        }
+
+        public LinkedListNode<Item> Find(int i)
+        {
+            if (first == null) return null;
+            LinkedListNode<Item> cur = first;
+            int pos = 1;
+            while (cur.next != null)
+            {
+                if (pos == i)
+                {
+                    return cur;
+                }
+                pos++;
+                cur = cur.next;
+            }
+
+            return null;
+        }
+
+
+        public void Append(LinkedListNode<Item> node)
+        {
+            if (first == null)
+            {
+                first = node;
+                return;
+            }
+
+            LinkedListNode<Item> last = first;
+
+            while (last.next != null)
+            {
+                last = last.next;
+            }
+            last.next = node;
+        }
+
+        public LinkedListNode<Item> Delete(int k)
+        {
+            if (first == null)
+            {
+                return null;
+            }
+
+            if (k == 1)
+            {
+                var res = first;
+                first = first.next;
+                return res;
+            }
+
+            Lib.LinkedListNode<Item> cur = first;
+            int pos = 2;
+
+            while (cur.next != null)
+            {
+                if (pos == k)
+                {
+                    var res = cur.next;
+                    cur.next = cur.next?.next;
+                    return res;
+                }
+                pos++;
+                cur = cur.next;
+            }
+
+            return null;
+        }
+
+        public int Length()
+        {
+            if (first == null)
+            {
+                return 0;
+            }
+
+            Lib.LinkedListNode<Item> cur = first;
+            int pos = 1;
+
+            while (cur.next != null)
+            {
+                pos++;
+                cur = cur.next;
+            }
+            return pos;
+        }
+
+        public void Print()
+        {
+            LinkedListNode<Item> cur = first;
+            while (cur.next != null)
+            {
+                Console.Write("{0} ", cur.item);
+                cur = cur.next;
+            }
+        }
+    }
+
+    public class LinkedListNode<Item>
+    {
+        public Item item;
+        public LinkedListNode<Item> next;
+
+        public LinkedListNode()
+        {
+        }
+
+        public LinkedListNode(Item item)
+        {
+            this.item = item;
+        }
+    }
+
+}
