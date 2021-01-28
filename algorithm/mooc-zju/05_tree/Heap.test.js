18192e83b9cebde523bca1df4e96da00038a6d55
Li Yu <edenliyu@outlook.com>
2021-01-28 17:48:52 +0800

    algo: add mocha test debug script

--- algorithm/mooc-zju/05_tree/Heap.test.js
+++ algorithm/mooc-zju/05_tree/Heap.test.js
@@ -6,14 +6,10 @@ const {
 describe('test createMaxHeap function', function () {
   let heap;
   it('heap obj should has right properties', function () {
-    heap = createMaxHeap([1, 2, 3, 4, 5], 100);
-    expect(heap.size).to.be.equal(5);
+    heap = createMaxHeap([4, 3, 9, 2, 8, 1, 7, 6, 5, 10], 100);
+    expect(heap.size).to.be.equal(10);
     expect(heap.capacity).to.be.equal(100);
-    expect(heap.elements[1]).to.be.equal(5);
-    expect(heap.elements[2]).to.be.equal(4);
-    expect(heap.elements[3]).to.be.equal(3);
-    expect(heap.elements[4]).to.be.equal(1);
-    expect(heap.elements[5]).to.be.equal(2);
+    expect(heap.elements.slice(1)).to.deep.equal([10, 8, 9, 6, 4, 1, 7, 2, 5, 3])
   });
 
   it('should create an empty heap', function () {
