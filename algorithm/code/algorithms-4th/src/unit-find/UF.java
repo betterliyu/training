8dc998dc8e4f338447a979281fd6ae8c492f3f26
Li Yu <edenliyu@outlook.com>
2021-04-07 22:55:18 +0800

    add algs-java project

---
+++ algorithm/code/algorithms-4th/src/unit-find/UF.java
@@ -0,0 +1,5 @@
+public class UF {
+  public static void main(String[] args) {
+      System.out.print("I am Hello Class, which is a main class");
+  }
+}
---
+++ algorithm/code/algorithms-java/src/Fundamentals/UF.java
@@ -0,0 +1,7 @@
+package Fundamentals;
+
+public class UF {
+    public static void main(String[] args) {
+        System.out.println("Long time to see! Java");
+    }
+}
